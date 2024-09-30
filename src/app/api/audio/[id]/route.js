10bcf6a3e946978c6e0c9f7262081c60959ad4d3
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2024-09-30 09:42:30 -0400

    setup: added api route for audio, image, text and playlist

---
+++ src/app/api/audio/[id]/route.js
@@ -0,0 +1,6 @@
+export const GET = async () => {
+  console.log("Get by id");
+};
+export const POST = async () => {
+  console.log("Post by id");
+};
---
+++ src/app/api/playlist/route.js
@@ -0,0 +1,6 @@
+export const GET = async () => {
+  console.log("Get");
+};
+export const POST = async () => {
+  console.log("Post");
+};
