06a78ab05f95d183c005918f1feef651754c2b82
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2024-09-30 22:49:07 -0400

    setup: removed test code

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,6 @@
 import React from "react";
 
 const HomePage = () => {
-  const test = async () => {
-    try {
-      const response = await fetch(`http://localhost:3000/api/audio/${1}`);
-      const data = await response.json();
-      console.log(data.message);
-    } catch (error) {
-      console.error("Error fetching video by ID:", error);
-    }
-  };
-
-  test();
   return <div>HomePage</div>;
 };
 
