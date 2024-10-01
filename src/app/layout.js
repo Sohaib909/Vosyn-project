33b2711c984aea36b5674415d094b082c99b18fa
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2024-09-30 22:47:22 -0400

    added mui theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,28 @@
+import { ThemeProvider } from "@mui/material";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { Inter } from "next/font/google";
+import theme from "@/utils/muiTheme";
 
 export const metadata = {
   title: "VosynVerse",
   description: "",
 };
 
+const inter = Inter({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["cyrillic", "greek", "latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body>
-        {children}
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
