8137258e0d84c5380394ad38d3f33521ceb88c42
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2024-10-03 09:31:42 -0400

    setup: fixed typo in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    REACT_APP_PROD_UR: "https://api.vosynverse.com",
+    REACT_APP_PROD_URL: "https://api.vosynverse.com",
     REACT_APP_LANGUAGE_DETECTION_URL: "https://langdetectapi.vosynverse.com"
   },
   experimental: {
