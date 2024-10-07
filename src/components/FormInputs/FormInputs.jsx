02d35d9be7bd68d96a929c7cdbcb9ee4befa7102
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2024-10-07 17:10:19 -0400

    forgot-password: Created components for form inputs and vosyn assist blob with text as well as a forgot-password page. Added custom colors to the mui theme

---
+++ src/components/FormInputs/FormInputs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FormInputs = () => {
+  return <div>FormInputs</div>;
+};
+
+export default FormInputs;
--- src/utils/muiTheme.js
+++ src/utils/muiTheme.js
@@ -10,6 +10,51 @@ const theme = createTheme({
   colorSchemes: {
     dark: true,
   },
+  palette: {
+    contrastThreshold: 4.5,
+    netural: {
+      25: "#FBFBFB",
+      50: "#F9F9F9",
+      100: "#EAEAEA",
+      200: "#DFDFDF",
+      300: "#C9C9C9",
+      400: "#9A9A9A",
+      500: "#777777",
+      600: "#575757",
+      700: "#3B3E3B",
+      800: "#1F1E1E",
+      900: "#121118",
+    },
+    primary: {
+      25: "#F7F9FF",
+      100: "#CBD7FD",
+      200: "#97AFFB",
+      300: "#7595FA",
+      400: "#527AF9",
+      main: "#324DA3",
+      550: "#244095",
+      600: "#010922",
+      700: "#121118",
+    },
+    secondary: {
+      100: "#E8F8FE",
+      200: "#BAE9FB",
+      300: "#A3E1FA",
+      main: "#41C4FA",
+      500: "#1391C2",
+      600: "#0E6D92",
+      700: "#0A4861",
+    },
+    tertiary: {
+      100: "#FBE9F9",
+      200: "#F2BEEE",
+      300: "#EDA8E8",
+      main: "#E57DDC",
+      500: "#E900CC",
+      600: "#7F1776",
+      700: "#3F0B3B",
+    },
+  },
 });
 
 export default theme;
