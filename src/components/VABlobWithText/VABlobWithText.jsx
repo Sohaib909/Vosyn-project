ca31e0792df1ff601348d57df45ae99cd9a5be26
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2024-10-10 19:57:11 -0400

    forgot-password: added blob animation

--- src/components/VABlobWithText/VABlobWithText.jsx
+++ src/components/VABlobWithText/VABlobWithText.jsx
@@ -3,15 +3,38 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import Image from "next/image";
 
+import blob1 from "@/components/VABlobWithText/blob-1-opacity-55.gif";
+import blob2 from "@/components/VABlobWithText/blob-2-opacity-46.gif";
+import blob3 from "@/components/VABlobWithText/blob-3-opacity-54.gif";
+
+import styles from "./VABlobWithText.module.css";
+
 const VABlobWithText = ({ text }) => {
   return (
     <Box sx={{ display: "flex", columnGap: "2rem" }}>
-      <Image
-        src={"https://placehold.co/60x60.png"}
-        width={60}
-        height={60}
-        alt="placeholder image"
-      />
+      <Box sx={{ position: "relative", width: "60px", height: "60px" }}>
+        <Image
+          src={blob1}
+          width={60}
+          height={60}
+          alt="placeholder image"
+          className={styles.blobImage}
+        />
+        <Image
+          src={blob2}
+          width={60}
+          height={60}
+          alt="placeholder image"
+          className={styles.blobImage}
+        />
+        <Image
+          src={blob3}
+          width={60}
+          height={60}
+          alt="placeholder image"
+          className={styles.blobImage}
+        />
+      </Box>
       <Typography
         sx={{
           color: "var(--mui-palette-netural-200)",
