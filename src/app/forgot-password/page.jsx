b79c8e3277fac653ab640def50429357bd03672a
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2024-10-10 18:54:53 -0400

    forgot-password: update to error handeling

--- src/app/forgot-password/page.jsx
+++ src/app/forgot-password/page.jsx
@@ -1,48 +1,101 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 import { Box, Button, TextField, Typography } from "@mui/material";
 import Link from "next/link";
 
+import StatusNotification from "@/components/StatusNotification/StatusNotification";
 import VABlobWithText from "@/components/VABlobWithText/VABlobWithText";
 
 import styles from "./page.module.css";
 
 const ForgotPassword = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const [message, setMessage] = useState();
+  const [severity, setSeverity] = useState();
+
+  const submitHandler = () => {
+    if (errors?.email?.message) {
+      setSeverity("error");
+      setMessage(errors?.email?.message);
+
+      return;
+    }
+
+    setSeverity("success");
+    setMessage("A verification email has been sent. Please check your inbox.");
+  };
+
+  const handleCloseNotification = () => {
+    setSeverity("");
+    setMessage("");
+  };
+
   return (
     <Box component="main" className={styles.pageContainer}>
       <Box className={styles.inputContainer}>
         <VABlobWithText text="Forgot your password? Don't worry! Just enter your email." />
 
-        <Box component="form" className={styles.input}>
-          <Typography>Email:</Typography>
-
-          <TextField
-            id="outlined-basic"
-            size="normal"
-            aria-label="email-input"
-            sx={{
-              width: "100%",
-              backgroundColor: "var(--mui-palette-netural-200)",
-              borderRadius: "5px",
-            }}
-          />
-        </Box>
-
-        <Button
-          variant="contained"
-          size="large"
-          sx={{
-            background: "var(--mui-palette-primary-400)",
-            paddingY: "1rem",
-          }}
+        <Box
+          component="form"
+          sx={{ display: "flex", flexDirection: "column", rowGap: "2rem" }}
+          onSubmit={handleSubmit(submitHandler)}
         >
-          Send Reset Link
-        </Button>
+          <Box className={styles.input}>
+            <Typography>Email:</Typography>
+
+            <TextField
+              id="outlined-basic"
+              size="normal"
+              aria-label="email-input"
+              sx={{
+                width: "100%",
+                backgroundColor: "var(--mui-palette-netural-200)",
+                borderRadius: "5px",
+              }}
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "Please enter a valid email address",
+                },
+              })}
+              error={!!errors.email}
+            />
+          </Box>
+
+          <Button
+            variant="contained"
+            size="large"
+            sx={{
+              background: "var(--mui-palette-primary-400)",
+              paddingY: "1rem",
+            }}
+            type="submit"
+          >
+            Send Reset Link
+          </Button>
+        </Box>
 
         <Link href="/?auth=login" className={styles.goBackBtn}>
           Back to Login
         </Link>
       </Box>
+
+      {message && (
+        <StatusNotification
+          severity={severity}
+          message={message}
+          onClose={handleCloseNotification}
+        />
+      )}
     </Box>
   );
 };
