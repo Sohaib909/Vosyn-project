676519ac05c61cb8e15e841d2ab5d66707ade209
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2024-10-16 17:47:45 -0400

    auth page intergration: added page style and layout

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,7 +1,38 @@
+"use client";
+
 import React from "react";
 
+import logo from "@/Images/Logos/vosyn_logo_long.png";
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+
+import styles from "./page.module.css";
+
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  const quary = useSearchParams();
+  const type = quary.get("type");
+
+  const getAuthComponent = () => {
+    if (type === "login") return <Box>Login</Box>;
+
+    return <Box>Signup</Box>;
+  };
+
+  return (
+    <Box component="main" className={styles.authContainer}>
+      {getAuthComponent()}
+
+      <Box className={styles.textContainer}>
+        <Box sx={{ height: "15%", width: "100%", position: "relative" }}>
+          <Image src={logo} className={styles.logo} />
+        </Box>
+        <Typography sx={{ fontSize: "larger" }}>
+          The Voice of Tomorrow
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default AuthPage;
