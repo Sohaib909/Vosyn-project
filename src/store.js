0add60e507bc79d270ded67e201ca04b202a2b1a
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2024-10-16 10:33:38 -0400

    forgot password: moved store.js inside src and fixed the relative import

--- src/store.js
+++ src/store.js
@@ -2,19 +2,19 @@ import statusNotificationSlice from "@/reduxSlices/statusNotificationSlice";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 const combineReducer = combineReducers({
-  statusNotification: statusNotificationSlice
-})
+  statusNotification: statusNotificationSlice,
+});
 
 const rootReducer = (state, action) => {
   if (action.type === "auth/logout") {
-    state = undefined
+    state = undefined;
   }
 
   return combineReducer(state, action);
-}
+};
 
 const store = configureStore({
   reducer: rootReducer,
-})
+});
 
 export default store;
