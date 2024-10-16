75ddc49d010a9f1a07b2809c4ea4664d81f9ef98
NolanChan1 <nolanchan1@gmail.com>
2024-10-16 17:46:24 -0600

    Add Signup form component + API route

--- src/constants/URLs/constants.js
+++ src/constants/URLs/constants.js
@@ -1,3 +1,4 @@
 const BASE_URL = process.env.REACT_APP_PROD_URL;
 
+export const SIGNUP_URL = `${BASE_URL}/user/register/`;
 export const RESET_PASSWORD_REQUEST = `${BASE_URL}/user/reset-password-request/`;
