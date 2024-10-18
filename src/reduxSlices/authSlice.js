233c54886ac40b676d0a55baed09dbaecc80bf1e
Sargunan Gurumurthy <sargunan1213@gmail.com>
2024-10-18 17:07:30 -0400

    Fixing PR

---
+++ src/reduxSlices/authSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setLoggedIn: (state, action) => {
+      state.isLoggedIn = action.payload;
+    },
+    logout: () => {},
+  },
+});
+
+export const { setLoggedIn, logout } = authSlice.actions;
+
+export default authSlice.reducer;
