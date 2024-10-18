d0edfc5e4d57c8ee384824c9e20cc0bf9c2b34eb
NolanChan1 <nolanchan1@gmail.com>
2024-10-17 19:44:23 -0600

    Add TermsAgreementModal + VerifyAccount

--- src/constants/URLs/constants.js
+++ src/constants/URLs/constants.js
@@ -2,3 +2,4 @@ const BASE_URL = process.env.REACT_APP_PROD_URL;
 
 export const SIGNUP_URL = `${BASE_URL}/user/register/`;
 export const RESET_PASSWORD_REQUEST = `${BASE_URL}/user/reset-password-request/`;
+export const RESEND_VERIFICATION_URL = `${BASE_URL}/user/resend-verification-email/`;
