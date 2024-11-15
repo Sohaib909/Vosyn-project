ad9f35d6fb6d57847eb453bbdf414b1bba5cb2b4
nandini-parekh <parekhnandini25@gmail.com>
2024-11-15 16:27:52 -0500

    removed more button from jump in section

--- src/components/MediaDisplay/GeneralTab/SectionHeader/SectionHeader.jsx
+++ src/components/MediaDisplay/GeneralTab/SectionHeader/SectionHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Box, Grid2, Typography } from "@mui/material";
 
-const SectionHeader = ({ heading, subheading, handleClick }) => {
+const SectionHeader = ({ heading, subheading }) => {
   return (
     <Grid2 size={12} sx={{ height: "fit-content" }}>
       <Typography variant="h6" sx={{ fontWeight: "bold" }}>
@@ -17,13 +17,13 @@ const SectionHeader = ({ heading, subheading, handleClick }) => {
         }}
       >
         <Typography variant="subtitle1">{subheading}</Typography>
-        <Typography
+        {/* <Typography
           variant="subtitle1"
           sx={{ textDecoration: "underline", cursor: "pointer" }}
           onClick={handleClick}
         >
           more
-        </Typography>
+        </Typography> */}
       </Box>
     </Grid2>
   );
