3c592a6f7b3f40cca00c982bec0aab7c57734fd9
Kweku Odum <amankwaodum@gmail.com>
2024-11-19 19:32:22 +0100

    importing saved content

--- src/app/(dashboard)/(playlist)/playlist/page.jsx
+++ src/app/(dashboard)/(playlist)/playlist/page.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import SinglePlaylist from "@/components/Playlist/SinglePlaylist/SinglePlaylist";
+
 const PlaylistPage = () => {
-  return <></>;
+  return (
+    <>
+      <SinglePlaylist />
+    </>
+  );
 };
 
 export default PlaylistPage;
