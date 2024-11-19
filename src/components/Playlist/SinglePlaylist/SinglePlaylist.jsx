0d0b4629fd2813dc029154589c02924d880b6995
Kweku Odum <amankwaodum@gmail.com>
2024-11-19 23:48:19 +0100

    modified the single sort playlist to include the sort by type functionality

--- src/components/Playlist/SinglePlaylist/SinglePlaylist.jsx
+++ src/components/Playlist/SinglePlaylist/SinglePlaylist.jsx
@@ -2,11 +2,19 @@
 
 import React from "react";
 
+import useQueryParam from "@/hooks/useQueryParam";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import PlayArrowOutlinedIcon from "@mui/icons-material/PlayArrowOutlined";
 import ShareIcon from "@mui/icons-material/Share";
-import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Link,
+  Typography,
+} from "@mui/material";
 
 const videos = [
   {
@@ -47,112 +55,171 @@ const videos = [
   },
 ];
 
+// Grouping logic for sorting by type
+const groupByType = (data) => {
+  const grouped = {
+    Videos: [],
+    Audio: [],
+    Text: [],
+  };
+
+  data.forEach((item) => {
+    if (item.type.includes("Video")) {
+      grouped.Videos.push(item);
+    } else if (item.type.includes("Audio")) {
+      grouped.Audio.push(item);
+    } else {
+      grouped.Text.push(item); // Articles and PDFs fall under Text
+    }
+  });
+
+  return grouped;
+};
+
 const SinglePlaylist = ({ data = videos }) => {
+  const groupedData = groupByType(data);
+
+  const { getAllParams } = useQueryParam();
+  const params = getAllParams();
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: "3vh" }}>
-      {data.map((item, i) => (
-        <Card
-          key={i}
-          sx={{
-            borderRadius: "12px",
-            maxWidth: "100%",
-            cursor: "pointer",
-            display: "flex",
-            minWidth: "fit-content",
-            alignItems: "center",
-            gap: 0,
-          }}
-        >
-          <Box sx={{ position: "relative" }}>
-            <CardMedia
-              component="img"
-              image={item.image}
-              sx={{
-                borderRadius: "16px",
-                maxWidth: "20rem",
-                "@media (max-width: 1400px)": {
-                  maxWidth: "30rem",
-                },
-              }}
-            />
-            {(item.type === "MP4 Video" || item.type === "Video Playlist") && (
-              <PlayArrowOutlinedIcon
-                sx={{
-                  position: "absolute",
-                  top: "50%",
-                  left: "50%",
-                  transform: "translate(-50%, -50%)",
-                  backgroundColor: "rgba(0, 0, 0, 0.6)",
-                  borderRadius: "50%",
-                }}
-              />
-            )}
-          </Box>
-
-          <CardContent
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-              position: "relative",
-              rowGap: "1rem",
-              width: "100%",
-            }}
-          >
-            <Box sx={{ position: "absolute", top: "5%", right: "2%" }}>
-              <ShareIcon fontSize="small" sx={{ mr: "0.5vw" }} />
-              <BookmarkIcon fontSize="small" sx={{ mr: "0.5vw" }} />
-              <MoreVertIcon fontSize="small" />
-            </Box>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                pr: "20%",
-                pt: "2vh",
-              }}
-            >
-              <Typography
-                variant="p"
-                sx={{ fontWeight: "bold", fontSize: "large" }}
-              >
-                {item.title}
-              </Typography>
-              <Typography
-                variant="p"
-                sx={{ fontWeight: "bold", fontSize: "large" }}
-              >
-                {item.type}
-              </Typography>
-            </Box>
+      {Object.entries(groupedData).map(([key, items]) => (
+        <Box key={key}>
+          {params.sort === "type" && (
             <Box
               sx={{
                 display: "flex",
                 flexDirection: "row",
                 justifyContent: "space-between",
-                pt: "1vh",
+                alignItems: "center",
               }}
             >
               <Typography
-                variant="caption"
-                sx={{ opacity: "0.7", fontSize: "medium" }}
+                variant="h5"
+                sx={{ fontWeight: "bold", textTransform: "capitalize", mb: 2 }}
               >
-                {item.description}
+                {key}
               </Typography>
-              <Typography
-                variant="caption"
+
+              <Link
+                variant="p"
+                sx={{ mb: 2, fontWeight: "bold" }}
+                underline="always"
+                color="inherit"
+              >
+                More
+              </Link>
+            </Box>
+          )}
+
+          {items.map((item) => (
+            <Card
+              key={item.id}
+              sx={{
+                borderRadius: "12px",
+                maxWidth: "100%",
+                cursor: "pointer",
+                display: "flex",
+                minWidth: "fit-content",
+                alignItems: "center",
+                gap: 0,
+                mb: 2,
+              }}
+            >
+              <Box sx={{ position: "relative" }}>
+                <CardMedia
+                  component="img"
+                  image={item.image}
+                  sx={{
+                    borderRadius: "16px",
+                    maxWidth: "20rem",
+                    "@media (max-width: 1400px)": {
+                      maxWidth: "30rem",
+                    },
+                  }}
+                />
+                {(item.type === "MP4 Video" ||
+                  item.type === "Video Playlist") && (
+                  <PlayArrowOutlinedIcon
+                    sx={{
+                      position: "absolute",
+                      top: "50%",
+                      left: "50%",
+                      transform: "translate(-50%, -50%)",
+                      backgroundColor: "rgba(0, 0, 0, 0.6)",
+                      borderRadius: "50%",
+                    }}
+                  />
+                )}
+              </Box>
+
+              <CardContent
                 sx={{
-                  opacity: "0.7",
-                  pr: "2%",
-                  pt: "2vh",
-                  fontSize: "medium",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                  position: "relative",
+                  rowGap: "1rem",
+                  width: "100%",
                 }}
               >
-                {item.date}
-              </Typography>
-            </Box>
-          </CardContent>
-        </Card>
+                <Box sx={{ position: "absolute", top: "5%", right: "2%" }}>
+                  <ShareIcon fontSize="small" sx={{ mr: "0.5vw" }} />
+                  <BookmarkIcon fontSize="small" sx={{ mr: "0.5vw" }} />
+                  <MoreVertIcon fontSize="small" />
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    pr: "20%",
+                    pt: "2vh",
+                  }}
+                >
+                  <Typography
+                    variant="p"
+                    sx={{ fontWeight: "bold", fontSize: "large" }}
+                  >
+                    {item.title}
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    sx={{ fontWeight: "bold", fontSize: "large" }}
+                  >
+                    {item.type}
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    pt: "1vh",
+                  }}
+                >
+                  <Typography
+                    variant="caption"
+                    sx={{ opacity: "0.7", fontSize: "medium" }}
+                  >
+                    {item.description}
+                  </Typography>
+                  <Typography
+                    variant="caption"
+                    sx={{
+                      opacity: "0.7",
+                      pr: "2%",
+                      pt: "2vh",
+                      fontSize: "medium",
+                    }}
+                  >
+                    {item.date}
+                  </Typography>
+                </Box>
+              </CardContent>
+            </Card>
+          ))}
+        </Box>
       ))}
     </Box>
   );
