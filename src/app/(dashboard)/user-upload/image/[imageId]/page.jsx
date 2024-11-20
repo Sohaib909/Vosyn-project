422f2ed86529742695309ac12cb04612388f9ae2
Phlegm Mignon <dmah@ucdavis.edu>
2024-11-20 10:03:50 -0800

    ImagePage migration

---
+++ src/app/(dashboard)/user-upload/image/[imageId]/page.jsx
@@ -0,0 +1,113 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+import { Box } from "@mui/material";
+import Image from "next/image";
+
+import TextAndImageActionBtns from "@/components/TextAndImageActionBtns/TextAndImageActionBtns";
+import TranslationPanel from "@/components/TranslationPanel/TranslationPanel";
+import TranslationSlider from "@/components/TranslationPanel/TranslationPanelSlider/TranslationPanelSlider";
+
+import Original from "../../../../../../public/mediaFiles/Images/Original.png";
+import Translated from "../../../../../../public/mediaFiles/Images/Translated.png";
+
+const ImagePage = () => {
+  const [image, setImage] = useState(null); // Default to null or loading image
+
+  useEffect(() => {
+    // Simulate fetching the original and translated images from an API
+    const fetchImages = async () => {
+      const originalImage = Original;
+
+      const translatedImage = Translated;
+
+      // Simulate async API call delay
+      setTimeout(() => {
+        setImage({
+          original: originalImage,
+          translated: translatedImage,
+          current: translatedImage, // Initially show the translated image
+        });
+      }, 900); // Mock delay
+    };
+
+    fetchImages();
+  }, []);
+
+  const handleTranslation = () => {
+    if (image) {
+      setImage({
+        ...image,
+        current:
+          image.current === image.translated
+            ? image.original
+            : image.translated,
+      });
+    }
+  };
+
+  return (
+    <Box padding="1rem var(--mui-spacing-8)">
+      <Box
+        display="flex"
+        alignItems="start"
+        justifyContent="space-between"
+        flexDirection="row"
+        paddingTop="var(--mui-spacing-4)"
+      >
+        <Box display="flex" flexDirection="column" flex="1" mr="5rem">
+          <Box display="flex" flexDirection="column" justifyContent="flex-end">
+            {image && (
+              <TextAndImageActionBtns
+                showRate={true}
+                fileUrl={image.translated}
+              />
+            )}
+          </Box>
+          <Box
+            display="flex"
+            flex="1"
+            minHeight="70%"
+            justifyContent="center"
+            mt="1rem"
+            sx={{
+              border: "2px solid var(--mui-palette-neutral-25)",
+              p: 8,
+              borderRadius: "1rem",
+            }}
+          >
+            {image && <Image src={image.current} alt="uploaded image" />}
+          </Box>
+        </Box>
+
+        <Box maxWidth="350px" m="0 0 0 2rem">
+          {image && (
+            <TranslationPanel fileUrl={image.translated}>
+              <TranslationSlider handleTranslation={handleTranslation} />
+            </TranslationPanel>
+          )}
+          {/* Uncomment once ContextualInfo and Summary are migrated over */}
+          {/* <Box className="contextual-info-wrapper">
+            <ContextualInfo />
+          </Box>
+
+          {image.current === image.translated && (
+            <Box width="100%" m={isMobile && "1rem 0"}>
+              <Summary summary={`The Gold Dragon`} title="Translation" />
+            </Box>
+          )}
+
+          <Box width="100%" m={isMobile && "1rem 0"}>
+            <Summary
+              summary={`Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.`}
+              title="Image Description"
+            />
+          </Box> */}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default ImagePage;
