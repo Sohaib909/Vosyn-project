40cf4cf93d3dfdf2e17f31cb37570c49daaa704e
Aniruddhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-11-29 19:03:47 -0500

    Fixes build errors

--- src/app/api/account/account.js
+++ src/app/api/account/account.js
@@ -1,41 +1,37 @@
+import { ACCOUNT_LANGUAGE_URL } from "@/constants/URLs/constants";
 import axios from "axios";
-import { ACCOUNT_LANGUAGE_URL} from "@/constants/URLs/constants";
-
 
 const axiosInstance = axios.create();
 
-
 export const getAccountLanguage = async () => {
-    const url = ACCOUNT_LANGUAGE_URL;  
-  
-    try {
-      const token = localStorage.getItem("token");
-      const response = await axios.get(url, {
-        headers: {
-          Authorization: `Token ${token}`,
-          "Content-Type": "application/json",
-        },
-      });
-      return response.data;  
-    } catch (error) {
-      console.error("Error fetching user settings:", error);
-    }
-  };
+  const url = ACCOUNT_LANGUAGE_URL;
 
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user settings:", error);
+  }
+};
 
-
-  export const updateAccountLanguage = async (data) => {
-    try {
-        const token = localStorage.getItem("token"); 
-        const response = await axiosInstance.patch(ACCOUNT_LANGUAGE_URL, data, {
-          headers: {
-            Authorization: `Token ${token}`, 
-            "Content-Type": "application/json",
-          },
-        });
-        console.log(response.data);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
+export const updateAccountLanguage = async (data) => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axiosInstance.patch(ACCOUNT_LANGUAGE_URL, data, {
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
--- src/components/Account/LoginSecurity/LoginSecurity.jsx
+++ src/components/Account/LoginSecurity/LoginSecurity.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from "react";
-import styles from "./LoginSecurity.module.css"; // Updated for CSS modules
-import MuiIcon from "../../../utils/MuiIcon.js"; // Assuming MuiIcon is compatible with Next.js
+
+// Assuming MuiIcon is compatible with Next.js
 // Importing the necessary MUI components
-import { Box, Typography, Button} from '@mui/material';
+import { Box, Button, Typography } from "@mui/material";
+
+// Updated for CSS modules
+import MuiIcon from "../../../utils/MuiIcon.js";
+
+import styles from "./LoginSecurity.module.css";
 
 function LoginSecurityPage() {
-   const [isPopupVisible, setPopupVisible] = useState(false);
+  const [isPopupVisible, setPopupVisible] = useState(false);
 
-   const handleDeleteAccountClick = () => {
-     setPopupVisible(true);
-   };
+  const handleDeleteAccountClick = () => {
+    setPopupVisible(true);
+  };
 
-   const handleClosePopup = () => {
-     setPopupVisible(false);
-   };
+  const handleClosePopup = () => {
+    setPopupVisible(false);
+  };
 
   return (
     <Box className={styles["login-security-page"]}>
       <Typography variant="h1">Login and Security</Typography>
-      <Typography variant="body1" className={styles.subtitle}>Account access and protection</Typography>
+      <Typography variant="body1" className={styles.subtitle}>
+        Account access and protection
+      </Typography>
 
       <Box className={styles["settings-container"]}>
         <Typography variant="h2">Change personal setting</Typography>
@@ -26,8 +33,12 @@ function LoginSecurityPage() {
         <Box className={styles["setting-item"]}>
           <Box className={styles["icon-placeholder"]}></Box>
           <Box className={styles["setting-info"]}>
-            <Typography variant="body1" className={styles["setting-title"]}>Email</Typography>
-            <Typography variant="body1" className={styles["setting-value"]}>Vosynai2024@gmail.com</Typography>
+            <Typography variant="body1" className={styles["setting-title"]}>
+              Email
+            </Typography>
+            <Typography variant="body1" className={styles["setting-value"]}>
+              Vosynai2024@gmail.com
+            </Typography>
           </Box>
           <MuiIcon name="Edit" className={styles["edit-icon"]} />
         </Box>
@@ -35,8 +46,12 @@ function LoginSecurityPage() {
         <Box className={styles["setting-item"]}>
           <Box className={styles["icon-placeholder"]}></Box>
           <Box className={styles["setting-info"]}>
-            <Typography variant="body1" className={styles["setting-title"]}>Mobile phone</Typography>
-            <Typography variant="body1" className={styles["setting-value"]}>123 456 7890</Typography>
+            <Typography variant="body1" className={styles["setting-title"]}>
+              Mobile phone
+            </Typography>
+            <Typography variant="body1" className={styles["setting-value"]}>
+              123 456 7890
+            </Typography>
           </Box>
           <MuiIcon name="Edit" className={styles["edit-icon"]} />
         </Box>
@@ -44,13 +59,20 @@ function LoginSecurityPage() {
         <Box className={styles["setting-item"]}>
           <Box className={styles["icon-placeholder"]}></Box>
           <Box className={styles["setting-info"]}>
-            <Typography variant="body1" className={styles["setting-title"]}>Password</Typography>
-            <Typography variant="body1" className={styles["setting-value"]}>**********</Typography>
+            <Typography variant="body1" className={styles["setting-title"]}>
+              Password
+            </Typography>
+            <Typography variant="body1" className={styles["setting-value"]}>
+              **********
+            </Typography>
           </Box>
           <MuiIcon name="Edit" className={styles["edit-icon"]} />
         </Box>
 
-        <Button className={styles["delete-account"]} onClick={handleDeleteAccountClick}>
+        <Button
+          className={styles["delete-account"]}
+          onClick={handleDeleteAccountClick}
+        >
           Delete Account
         </Button>
       </Box>
@@ -58,7 +80,10 @@ function LoginSecurityPage() {
       {isPopupVisible && (
         <Box className={styles.popup}>
           <Box className={styles["popup-content"]}>
-          <Typography variant="body1"> Do you want to delete your account? </Typography>
+            <Typography variant="body1">
+              {" "}
+              Do you want to delete your account?{" "}
+            </Typography>
             <Box className={styles["popup-buttons"]}>
               <Button disabled>Yes</Button>
               <Button onClick={handleClosePopup}>No</Button>
