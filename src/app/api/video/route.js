b57284567c4a226678550da0f5723b173cc6cd0a
YugBandhara <ybandhara@gmail.com>
2024-11-29 21:50:06 -0500

    updated

--- src/app/api/video/route.js
+++ src/app/api/video/route.js
@@ -3,22 +3,40 @@ import axiosInstance from "@/utils/axiosInstance";
 import { NextResponse } from "next/server";
 
 export const GET = async (req) => {
-  const { searchParams } = new URL(req.url);
+  try {
+    const { searchParams } = new URL(req.url);
 
-  const data = {
-    sort_by: searchParams.get("sort_by") || "view_count",
-    limit: searchParams.get("limit") || 15,
-    page: searchParams.get("page") || 1,
-  };
+    const data = {
+      sort_by: searchParams.get("sort_by") || "view_count",
+      limit: searchParams.get("limit") || 15,
+      page: searchParams.get("page") || 1,
+    };
 
-  const queryParams = new URLSearchParams(data).toString();
-  const videoListUrl = `${VIDEO_LIST_URL}/?${queryParams}`;
+    const queryParams = new URLSearchParams(data).toString();
+    const videoListUrl = `${VIDEO_LIST_URL}/?${queryParams}`;
 
-  const res = await axiosInstance.get(videoListUrl);
+    const response = await axiosInstance.get(videoListUrl);
 
-  return NextResponse.json(res.data, { status: res.status });
+    return NextResponse.json(response.data, { status: response.status });
+  } catch (error) {
+    return NextResponse.json(
+      error?.response?.data || { message: "An error occurred" },
+      {
+        status: error?.response?.status || 500,
+      },
+    );
+  }
 };
 
 export const POST = async () => {
-  return NextResponse.json({ message: "Video post" });
+  try {
+    return NextResponse.json({ message: "Video post" });
+  } catch (error) {
+    return NextResponse.json(
+      error?.response?.data || { message: "An error occurred" },
+      {
+        status: error?.response?.status || 500,
+      },
+    );
+  }
 };
