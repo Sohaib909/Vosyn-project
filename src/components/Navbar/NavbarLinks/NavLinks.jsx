0e4fb9f553529e32ef47e1e94c11ff028e7479e7
Anmol Gupta <111723339+anmolgupta313@users.noreply.github.com>
2024-12-04 15:06:45 -0500

    I have commented out history notification and friend routes from the navlinks

--- src/components/Navbar/NavbarLinks/NavLinks.jsx
+++ src/components/Navbar/NavbarLinks/NavLinks.jsx
@@ -1,16 +1,15 @@
 "use client";
 
 import {
-  ACCOUNT_PAGE_ROUTE,
-  FRIENDS_PAGE_ROUTE,
-  HISTORY_PAGE_ROUTE,
-  NOTIFICATIONS_PAGE_ROUTE,
+  ACCOUNT_PAGE_ROUTE, // FRIENDS_PAGE_ROUTE,
+  // HISTORY_PAGE_ROUTE,
+  // NOTIFICATIONS_PAGE_ROUTE,
   PLAYLIST_PAGE_ROUTE,
   SETTINGS_PAGE_ROUTE,
 } from "@/constants/URLs/routes";
-import GroupOutlinedIcon from "@mui/icons-material/GroupOutlined";
-import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
-import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+// import GroupOutlinedIcon from "@mui/icons-material/GroupOutlined";
+// import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
+// import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
@@ -21,14 +20,14 @@ import { usePathname } from "next/navigation";
 import styles from "./NavLinks.module.css";
 
 const navbarLinks = [
-  { name: "Friends", href: FRIENDS_PAGE_ROUTE, icon: GroupOutlinedIcon },
-  { name: "History", href: HISTORY_PAGE_ROUTE, icon: HistoryOutlinedIcon },
+  // { name: "Friends", href: FRIENDS_PAGE_ROUTE, icon: GroupOutlinedIcon },
+  // { name: "History", href: HISTORY_PAGE_ROUTE, icon: HistoryOutlinedIcon },
   { name: "Playlists", href: PLAYLIST_PAGE_ROUTE, icon: PlaylistPlayIcon },
-  {
-    name: "Notifications",
-    href: NOTIFICATIONS_PAGE_ROUTE,
-    icon: NotificationsOutlinedIcon,
-  },
+  // {
+  //   name: "Notifications",
+  //   href: NOTIFICATIONS_PAGE_ROUTE,
+  //   icon: NotificationsOutlinedIcon,
+  // },
   { name: "Settings", href: SETTINGS_PAGE_ROUTE, icon: SettingsOutlinedIcon },
   { name: "Account", href: ACCOUNT_PAGE_ROUTE, icon: PersonOutlinedIcon },
 ];
