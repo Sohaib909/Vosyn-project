86413a6d1f16fda45446e012c3d50b211230768f
Sargunan Gurumurthy <sargunan1213@gmail.com>
2024-12-16 14:18:50 -0500

    Fixing reload issue for recommended videos

--- src/components/AudioVideoCommonComponents/SideDisplayContainer/SideDisplayContainer.jsx
+++ src/components/AudioVideoCommonComponents/SideDisplayContainer/SideDisplayContainer.jsx
@@ -10,7 +10,7 @@ import useSWR from "swr";
 
 // import ContextualInfo from "../ContextualInfo/ContextualInfo";
 // eslint-disable-next-line prettier/prettier
-import PlaylistCard from "../PlaylistCard/PlaylistCard";
+// import PlaylistCard from "../PlaylistCard/PlaylistCard";
 // eslint-disable-next-line prettier/prettier
 import RecommendedCard from "../RecommendedCard/RecommendedCard";
 
@@ -20,26 +20,23 @@ const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 const SideDisplayContainer = ({
   containerType = "recommended",
-  playlistId = null,
   title = "Recommended videos",
 }) => {
-  const getRandValue = () => Math.floor(Math.random() * 8) + 1;
-
   // Using SWR to fetch recommended videos
   const { data: videoListData, error: videoListsError } = useSWR(
     containerType === "recommended"
-      ? `/api/video?sort_by=view_count&limit=6&page=${getRandValue()}`
+      ? `/api/video?sort_by=view_count&limit=6&page=1`
       : null,
     fetcher,
   );
 
-  // Using SWR to fetch playlists
-  const { data: playlistData, error: playlistsError } = useSWR(
-    containerType === "playlist"
-      ? `/api/playlist/singlePlaylist/${playlistId}?limit=4&order_by=video_name&page=1`
-      : null,
-    fetcher,
-  );
+  // // Using SWR to fetch playlists
+  // const { data: playlistData, error: playlistsError } = useSWR(
+  //   containerType === "playlist"
+  //     ? `/api/playlist/singlePlaylist/${playlistId}?limit=4&order_by=video_name&page=1`
+  //     : null,
+  //   fetcher,
+  // );
 
   const { setStatus } = useStatusNotification();
 
@@ -49,72 +46,38 @@ const SideDisplayContainer = ({
     setStatus(`${videoListsError?.message}.`, "error");
   }
 
-  if (containerType === "playlist" && playlistsError) {
-    setStatus(`${playlistsError?.message}.`, "error");
-  }
-
   const recommendedVideoClicked = (videoId) => {
     router.push(`/video/${videoId}`);
   };
 
   return (
     <>
-      {containerType === "playlist" && (
-        <Grid2 item size={12} className={styles.sectionContainer}>
-          <Grid2 item size={12} className={styles.header}>
-            <Box>
-              <Typography variant="body1">Playlist name</Typography>
-              <Typography variant="caption" sx={{ opacity: "0.7" }}>
-                {playlistData?.data?.length} videos
-              </Typography>
-            </Box>
-          </Grid2>
-
-          <Grid2
-            container
-            item
-            size={12}
-            spacing={2}
-            className={styles.contentContainer}
-          >
-            {playlistData?.data?.map((item) => (
-              <PlaylistCard
-                key={item?.content_object?.id}
-                playlistItem={item?.content_object}
-              />
-            ))}
-          </Grid2>
+      <Grid2 item size={12} className={styles.sectionContainer}>
+        <Grid2 item size={12} className={styles.header}>
+          <Box>
+            <Typography variant="body1">{title}</Typography>
+            <Typography variant="caption" sx={{ opacity: "0.7" }}>
+              {videoListData?.length} videos
+            </Typography>
+          </Box>
         </Grid2>
-      )}
 
-      {containerType === "recommended" && (
-        <Grid2 item size={12} className={styles.sectionContainer}>
-          <Grid2 item size={12} className={styles.header}>
-            <Box>
-              <Typography variant="body1">{title}</Typography>
-              <Typography variant="caption" sx={{ opacity: "0.7" }}>
-                {videoListData?.length} videos
-              </Typography>
-            </Box>
-          </Grid2>
-
-          <Grid2
-            container
-            item
-            size={12}
-            spacing={2}
-            className={styles.contentContainer}
-          >
-            {videoListData?.map((item) => (
-              <RecommendedCard
-                key={item?.document?.id}
-                recommendedItem={item?.document}
-                onClick={recommendedVideoClicked}
-              />
-            ))}
-          </Grid2>
+        <Grid2
+          container
+          item
+          size={12}
+          spacing={2}
+          className={styles.contentContainer}
+        >
+          {videoListData?.map((item) => (
+            <RecommendedCard
+              key={item?.document?.id}
+              recommendedItem={item?.document}
+              onClick={recommendedVideoClicked}
+            />
+          ))}
         </Grid2>
-      )}
+      </Grid2>
     </>
   );
 };
